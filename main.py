e74d34b2192ee7a7e525b4ac3d42c2503065f132
joemar2 <joemar2@cisco.com>
2024-09-10 11:46:58 -0400

    7821 V26 now supported with ENT firmware 14.3.1

--- main.py
+++ main.py
@@ -477,8 +477,8 @@ def cloudReady(result_dict, full_details, typemodel_dict):
         try:
             hw_ver = full_details[devicename]['hw_ver']
 
-            if model == typemodel_dict['7821'] and hw_ver != 'unknown' and hw_ver >= 'V03':  # 7821 (V03-V24)
-                if hw_ver == 'V25' or hw_ver == 'V26':
+            if model == typemodel_dict['7821'] and hw_ver != 'unknown' and hw_ver >= 'V03':  # 7821 (V03-V24, V26)
+                if hw_ver == 'V25':
                     cloud_ready = 'No'
                 else:
                     cloud_ready = 'Yes'
